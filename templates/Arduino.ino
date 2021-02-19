ea9b409a36fa258424721494b3899298eb4b39ac
Elang S.P <54803583+elangse@users.noreply.github.com>
2021-02-19 11:34:50 +0700

    Menambahkan template Arduino

---
+++ templates/Arduino.ino
@@ -0,0 +1,24 @@
+/*-----( Import Library yang Dibutuhkan )-----*/
+/*-----( Deklarasi Konstanta / Variabel "Nomor Pin" )-----*/
+/*-----( Deklarasi Object )-----*/
+/*-----( Deklarasi Variabel Global )-----*/
+
+/***
+  Program setup() hanya berjalan 1x.
+  setup() sering digunakan m
+***/
+void setup()
+{
+
+}
+
+/***
+  Program loop() akan berjalan terus-menurus.
+  loop() adalah bagian utama Arduino
+***/
+void loop()
+{
+
+}
+
+/*-----( Deklarasi Custom Function)-----*/
